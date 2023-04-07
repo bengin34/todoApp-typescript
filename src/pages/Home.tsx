9a2eb99c69d52982bb90c410fc849ccd0cf99477
bengin34 <becaglar3434@gmail.com>
2023-04-07 14:51:59 +0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import InputForm from "../components/InputForm";
+import TodoList from "../components/TodoList";
+
+const url = "https://642ff761c26d69edc887bbf1.mockapi.io/todos";
+
+const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const getTodos = async () => {
+    try {
+      const { data } = await axios.get<TodoType[]>(url);
+      console.log(data);
+      setTodos(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <div className="main">
+      <InputForm />
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+
+export default Home;
